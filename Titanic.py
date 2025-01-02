4ee7eab0f896fde8f2abe5bffefaf81694a25fee
Mayowa Ayeni <66896350+Mayeni97@users.noreply.github.com>
2025-01-02 18:36:22 -0500

    inspections

--- Titanic.py
+++ Titanic.py
@@ -3,5 +3,9 @@ import pandas as pd
 train_data = pd.read_csv("data/train.csv")
 test_data = pd.read_csv("data/test.csv")
 
-
+# Inspecting data
+print("Missing values in train data:",("\n"),train_data.isnull().sum())
+print("Missing values in test data:", ("\n"),test_data.isnull().sum())
+print("Train Data Description",("\n"),train_data.describe())
+print("Test Data Description",("\n"),test_data.describe())
 
