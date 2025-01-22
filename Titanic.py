72df860220913be01a5f8160f2a272ae0685f8f7
Mayowa Ayeni <66896350+Mayeni97@users.noreply.github.com>
2025-01-21 21:13:58 -0500

    Refactor Titanic.py: remove unused imports, add family size feature, and update group mean calculations

--- Titanic.py
+++ Titanic.py
@@ -2,9 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 
-from sklearn.model_selection import OridnaryEncoder,OneHotEncoder
+# from sklearn.model_selection import OridnaryEncoder,OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline,make_pipeline
@@ -20,4 +19,11 @@ from sklearn.model_selection import cross_val_score,StratifiedGroupKFold,train_t
 train_data = pd.read_csv("data/train.csv")
 test_data = pd.read_csv("data/test.csv")
 
+# print(train_data.head(),train_data.info(),train_data.describe())
 
+print(train_data.groupby(["Pclass"], as_index=False) ["Survived"].mean())
+print(train_data.groupby(['SibSp'], as_index=False) ["Survived"].mean())
+print(train_data.groupby(['Parch'], as_index=False) ["Survived"].mean())
+
+train_data['FamilySize'] = train_data['SibSp'] + train_data['Parch'] +1
+test_data['FamilySize'] = test_data['SibSp'] + test_data['Parch'] + 1
