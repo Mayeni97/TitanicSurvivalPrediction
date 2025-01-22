3c77bb8b180b2119c2d22bdd3b09a82fd9da2918
Mayowa Ayeni <66896350+Mayeni97@users.noreply.github.com>
2025-01-21 19:16:03 -0500

    Add imports for machine learning models and preprocessing steps in Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -2,6 +2,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+%matplotlib inline
+
+from sklearn.model_selection import OridnaryEncoder,OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline,make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score,StratifiedGroupKFold,train_test_split,GridSearchCV
+
 
 train_data = pd.read_csv("data/train.csv")
 test_data = pd.read_csv("data/test.csv")
